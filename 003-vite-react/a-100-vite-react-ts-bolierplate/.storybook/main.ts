dbb684d48050ab9a6996ee10d03a1f376c644c0e
git-k <kgunu93@gmail.com>
2023-09-21 21:31:26 +0900

    add vite react boliler

---
+++ 003-vite-react/a-100-vite-react-ts-bolierplate/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  typescript: {
+    reactDocgen: 'react-docgen' // 👈 react-docgen configured here.
+  }
+};
+
+export default config;
